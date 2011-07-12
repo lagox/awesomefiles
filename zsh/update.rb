7fd78bab3839f358c59ca9b36eaeba07dcdb7653
Sasha Koss <kossnocorp@gmail.com>
2011-07-12 15:50:22 +0700

    Added .zshrc to zsh update script

--- zsh/update.rb
+++ zsh/update.rb
@@ -1,5 +1,10 @@
 module Zsh
   def self.update!
+    zshrc = File.expand_path('~/.zshrc')
+
+    File.delete(zshrc) if File.symlink?(zshrc)
+    File.symlink(File.expand_path('zshrc.zsh'), zshrc)
+
     omz_custom = File.expand_path('~/.oh-my-zsh/custom')
 
     File.delete(omz_custom) if File.symlink?(omz_custom)
---
+++ zsh/zshrc.zsh
@@ -0,0 +1,14 @@
+# Oh My Zsh path
+export ZSH=$HOME/.oh-my-zsh
+
+# Plugins
+plugins=(git)
+
+# Theme
+export ZSH_THEME="robbyrussell"
+
+# Start Oh My Zsh
+source $ZSH/oh-my-zsh.sh
+
+# PATH
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin
